8e29cf17440de8a8a93e6daf0ec3537d6c4f6335
gabriellesumner <gabbysumner1010@gmail.com>
2022-04-24 14:34:03 -0400

    add sim_move function

--- hand-of-the-king-main/ai_minimax.py
+++ hand-of-the-king-main/ai_minimax.py
@@ -6,30 +6,29 @@ import math
 
 def get_computer_move(board, cards, banners, turn):
     '''Randomly select a move from the set of valid moves.'''
-    moves = getvalidmoves(board)
-    return random.choice(moves)
+    return minimax(board, cards, banners, turn)
 
 
-def minimax(board, player):
+def minimax(board, cards, banners, turn):
     '''Returns the best action from a given state in the game for a specific player.'''
-    actions = getvalidmoves(board, player)
-    best = actions[0]
+    moves = getvalidmoves(board)
+    best = moves[0]
     # alpha = -math.inf
     # beta = math.inf
-    value = minvalue(board, player, best)
-    for action in actions[1:]:
-        v = minvalue(board, player, action)
+    value = minvalue(board, cards, banners, turn, best)
+    for move in moves[1:]:
+        v = minvalue(board, cards, banners, turn, move)
         if v > value:
-            best = action
+            best = move
             value = v
     return best
 
 
-def minvalue(board, player, action):
+def minvalue(board, cards, banners, turn, move):
     '''Returns the minimum utility available from a player taking an action on the current board.'''
     # Simulate the action of the current player
     #state = board.copy()
-    board[action[0], action[1]] = player
+    sim_move(board, cards, banners, turn, move)
     nextplayer = 3 - player
 
     # Check if we are in a terminal state
@@ -72,3 +71,8 @@ def maxvalue(board, player, action):
         #     return value
     board[action[0], action[1]] = 0
     return value
+
+
+def sim_move(board, cards, banners, turn, move):
+    '''Simulate a move'''
+    pass
